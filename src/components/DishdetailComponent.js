defce6e3df4041940ee71852af9166ce212c9ef8
Vibhuti Purohit <pandyavibhu@gmail.com>
2022-02-01 16:13:14 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,63 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    renderDish(dish) {
-        if(dish!=null) {
-            return (
-                <div key={dish.id}>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12 col-md-5 m-1">
-                                <Card>
-                                    <CardImg top src={dish.image} alt={dish.name} />
-                                    <CardBody>
-                                        <CardTitle>{dish.name}</CardTitle>
-                                        <CardText>{dish.description}</CardText>
-                                    </CardBody>
-                                </Card>
-                            </div> 
-                            <div className="col-12 col-md-5 m-1">
-                                    {this.renderComments(dish)}
-                            </div>
-                        </div>
-                    </div>
-                </div>  
-            );
-        }
-        else {
-            <div></div>
-        }
-    }
-
-    renderComments(dish){
+function RenderDish({dish}) {
+    if(dish!=null) {
         return (
-            <div>
-                <h4> Comments </h4>
-                {dish.comments.map((comment) => {
-                    return (
-                        <ListGroup>
-                            <ListGroupItem className="border-0">{comment.comment}</ListGroupItem>
-                            <ListGroupItem className="border-0"><p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p></ListGroupItem>
-                        </ListGroup>
-                    )     
-                })}
-            </div>
+            <div key={dish.id}>
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>  
         );
     }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
 
-    render() {
+function RenderComments({comments}) {
+    return (
+        <div>
+            <h4> Comments </h4>
+            {comments.map((comment) => {
+                return (
+                    <ListGroup>
+                        <ListGroupItem className="border-0">{comment.comment}</ListGroupItem>
+                        <ListGroupItem className="border-0"><p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p></ListGroupItem>
+                    </ListGroup>
+                );     
+            })}
+        </div>
+    );
+}
+
+const DishDetail = (props) => {
+    if(props.dish != null) {
         return (
             <div className="container">
-                { this.renderDish(this.props.dish) }
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.dish.comments} />
+                    </div>
+                </div>
             </div>
         );
     }
-
+    else {
+        return (
+            <div></div>
+        );
+    }
 }
 
 export default DishDetail;
